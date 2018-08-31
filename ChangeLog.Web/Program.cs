7b67ab27673325b3ba2b9413807723e781b4532c
Kyle Neumeier <kneumei@gmail.com>
2018-08-30 22:49:17 -0400

    beanstalk env vars

--- ChangeLog.Web/Program.cs
+++ ChangeLog.Web/Program.cs
@@ -14,16 +14,37 @@ namespace ChangeLog.Web
 	{
 		public static void Main(string[] args)
 		{
+			SetEbConfig();
 			BuildWebHost(args).Run();
 		}
 
 		public static IWebHost BuildWebHost(string[] args) =>
 			WebHost.CreateDefaultBuilder(args)
-				.ConfigureAppConfiguration((buildContext, config) =>
-				{
-					config.AddJsonFile(@"C:\Program Files\Amazon\ElasticBeanstalk\config\containerconfiguration", optional: true, reloadOnChange: true);
-				})
 				.UseStartup<Startup>()
 				.Build();
+
+		private static void SetEbConfig()
+		{
+			var tempConfigBuilder = new ConfigurationBuilder();
+
+			tempConfigBuilder.AddJsonFile(
+				@"C:\Program Files\Amazon\ElasticBeanstalk\config\containerconfiguration",
+				optional: true,
+				reloadOnChange: true
+			);
+
+			var configuration = tempConfigBuilder.Build();
+
+			var ebEnv =
+				configuration.GetSection("iis:env")
+					.GetChildren()
+					.Select(pair => pair.Value.Split(new[] { '=' }, 2))
+					.ToDictionary(keypair => keypair[0], keypair => keypair[1]);
+
+			foreach (var keyVal in ebEnv)
+			{
+				Environment.SetEnvironmentVariable(keyVal.Key, keyVal.Value);
+			}
+		}
 	}
 }
